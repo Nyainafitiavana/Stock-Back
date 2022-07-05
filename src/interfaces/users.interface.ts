833287e42abdfb2d3d45c36f2df542e8dda53e96
NyAinaFitiavana <ainafitiavana0120@gmail.com>
2022-07-05 13:47:05 +0300

    add news property in users interface

--- src/interfaces/users.interface.ts
+++ src/interfaces/users.interface.ts
@@ -1,5 +1,11 @@
+import { Roles } from './roles.interface';
+
 export interface User {
   id: number;
   email: string;
   password: string;
+  userName: string;
+  telephone: string;
+  adresse: string;
+  role: Roles;
 }
