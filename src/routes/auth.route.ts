b8cc5224c210842f81c704e2bcd30401bde49b86
NyAinaFitiavana <ainafitiavana0120@gmail.com>
2022-07-06 11:40:54 +0300

    securise route category

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Router } from 'express';
 import AuthController from '@controllers/auth.controller';
 import { CreateUserDto, CreateLoginDto } from '@dtos/users.dto';
