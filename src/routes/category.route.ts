5972878f82abfe517f4f717d8f5649eded0ed52f
NyAinaFitiavana <ainafitiavana0120@gmail.com>
2022-07-06 13:45:23 +0300

    add new security category route

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -5,6 +5,7 @@ import validationMiddleware from '@middlewares/validation.middleware';
 import CategoryController from '@/controllers/category.controller';
 import { CreateCategoryDto } from '@/dtos/category.dto';
 import securityMiddleware from '../middlewares/securityToken.middleware';
+import authMiddleware from '@middlewares/auth.middleware';
 
 class CategoryRoute implements Routes {
   public path = '/category';
@@ -16,8 +17,8 @@ class CategoryRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.categoryController.getCategory);
-    this.router.get(`${this.path}/:id(\\d+)`, this.categoryController.findCategoryById);
+    this.router.get(`${this.path}`,authMiddleware, this.categoryController.getCategory);
+    this.router.get(`${this.path}/:id(\\d+)`,authMiddleware, this.categoryController.findCategoryById);
     this.router.post(`${this.path}`,securityMiddleware, validationMiddleware(CreateCategoryDto, 'body'), this.categoryController.createCategory);
     this.router.put(`${this.path}/:id(\\d+)`,securityMiddleware, validationMiddleware(CreateCategoryDto, 'body', true), this.categoryController.updateCategory);
     this.router.delete(`${this.path}/:id(\\d+)`,securityMiddleware, this.categoryController.deleteCategory);
