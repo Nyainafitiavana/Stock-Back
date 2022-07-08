b632b4c4d8e2d42c1bf6e564089f02fba7568739
Ny Aina Project <ainafitiavana.project@gmail.com>
2022-07-08 15:23:39 +0300

    modification route

--- src/routes/stock.route.ts
+++ src/routes/stock.route.ts
@@ -18,7 +18,7 @@ class StockRoute implements Routes {
   private initializeRoutes() {
     this.router.get(`${this.path}/stocks`, securityMiddleware, this.routeController.getAllStock);
      this.router.get(`${this.path}/stocks/:id(\\d+)`, securityMiddleware, this.routeController.getStock);
-     this.router.get(`${this.path}/stocks-seuil`, securityMiddleware, this.routeController.getSeuilStock);
+     this.router.get(`${this.path}/stocks-rupture`, securityMiddleware, this.routeController.getSeuilStock);
      this.router.post(`${this.path}/stocks`, securityMiddleware, validationMiddleware(CreatestockDto, 'body'), this.routeController.createStock);
      
   }
